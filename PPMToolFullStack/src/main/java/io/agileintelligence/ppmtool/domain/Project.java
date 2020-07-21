b6e5fafb41d008abe73b5f9f90d2770e35427593
ALELI ZAPANTA <lhaeilalee@yahoo.com>
2020-07-21 14:22:29 +0800

    Project object validation

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -20,27 +20,20 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 	@NotBlank(message = "Project Name is required")
 	private String projectName;
-	
 	@NotBlank(message = "Project Identifier is required")
 	@Size(min=4, max=5, message = "Please use 4 to 5 characters")
 	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
-	
 	@NotBlank(message = "Project description is required")
 	private String description;
-	
 	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date startDate;
-	
 	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date endDate;
-	
 	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date createdAt;
-	
 	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updatedAt;
 	
